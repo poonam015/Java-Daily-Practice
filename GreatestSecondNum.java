ced80b2312df25c0ce822b4beff3cf5061ae5309
Poonam Shinde <123967206+poonam015@users.noreply.github.com>
2023-02-16 10:00:33 +0530

    16/02/2023
    Greatest 2nd Number

---
+++ GreatestSecondNum.java
@@ -0,0 +1,15 @@
+//Print Greatest of two numbers using Conditional operator in java
+
+class Conditional 
+{
+public static void main(String[] args) 
+{
+//Two input numbers 
+int a = 50;
+int b = 95;
+
+int result = ( a > b )? a:b;
+
+System.out.println("Here is the Greatest of Three Number:"+result);
+}
+}
