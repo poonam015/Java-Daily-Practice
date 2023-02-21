d48f4a453166697fd53070b61e62d29d9730d6d4
Poonam Shinde <123967206+poonam015@users.noreply.github.com>
2023-02-21 23:33:34 +0530

    Practice on 21/02/2023
    Practice on 21/02/2023

---
+++ Single_Inheritance.java
@@ -0,0 +1,21 @@
+class ambmall extends Inox
+{
+	public static void main(String ps[])
+	{
+		ambmall am=new ambmall();
+		am.Screen1();
+		am.Screen2();
+	}
+}
+
+class Inox
+{
+	public void Screen1()
+	{
+		System.out.println("Screen 1 has : Ironman");
+	}
+	public void Screen2()
+	{
+		System.out.println("Screen 2 has : Thor");
+	}
+}
