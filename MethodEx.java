84382124232df60758b977f6f63ce39933f13b4c
Poonam Shinde <123967206+poonam015@users.noreply.github.com>
2023-03-11 12:43:15 +0530

    13/03/2023

---
+++ MethodEx.java
@@ -0,0 +1,16 @@
+class methodex{
+	
+   public static void main(String args[])
+	{
+
+		System.out.println("Hello World!");
+		methodex mex=new methodex();
+		mex.display();
+	}	
+
+	public void display(){
+
+		System.out.println("Welcome To the Method");
+		}
+
+}
